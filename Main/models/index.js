28916a7d5b3619cf05cb26a0a38b0715d88c28fa
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-03 23:59:06 +1000

    Product tage and tag added in models.

--- Main/models/index.js
+++ Main/models/index.js
@@ -5,10 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany()
 
 // Tags belongToMany Products (through ProductTag)
 
