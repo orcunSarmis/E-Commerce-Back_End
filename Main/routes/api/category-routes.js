c1af1b4ab280a5f916bf8b0b00a656ae9f1318f2
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-04 19:53:50 +1000

    Delete added in to routes.

--- Main/routes/api/category-routes.js
+++ Main/routes/api/category-routes.js
@@ -63,7 +63,17 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
+  try {
   // delete a category by its `id` value
+const categoryData = await Category.destroy({ where: { id: req.params.id }});
+if (!categoryData) {
+  res.status(404).json({ message: 'No category found with this id' });
+  return;
+}
+res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  } 
 });
 
 module.exports = router;
