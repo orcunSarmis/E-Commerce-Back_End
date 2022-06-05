64bd69a88ec23c657d9c82e2d3704d6023fefaad
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-05 19:59:23 +1000

    tag routers findAl added.

--- Main/models/index.js
+++ Main/models/index.js
@@ -6,19 +6,20 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL'
+  // onDelete: 'SET NULL'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id'
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
--- Main/routes/api/product-routes.js
+++ Main/routes/api/product-routes.js
@@ -4,21 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// router.get('/', async (req, res) => {
+//   // find all products
+//   // be sure to include its associated Category and Tag data
+//   try {
+//     const productData = Product.findAll({ 
+//       include: [{ model: Category, 
+//         attributes: ['id', 'category_name']}],
+//   {
+//     model:Tag, 
+//     attributes: ['id', 'tag_name']
+//   }
+  
+// });
+// res.status(200).json(productData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get('/', async (req,res) => {
   try {
-    const productData = Product.findAll({ 
-      include: [{ model: Category, 
-        attributes: ['id', 'category_name']},
-  {
-    model:Tag, 
-    attributes: ['id', 'tag_name']
-  }
-  ]
-});
-res.status(200).json(productData);
-  } catch (err) {
+    const productData = await Product.findAll({
+      include: [
+        Category, 
+        {
+          model: Tag, 
+          through: ProductTag
+        }
+      ]
+    });
+
+    res.status(200).json(productData);
+
+  } catch (error) {
     res.status(500).json(err);
   }
 });
