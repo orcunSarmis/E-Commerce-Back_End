63b869f69243f812063bb567dd1b84d0ed8a3e2a
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-05 21:43:47 +1000

    Update added into tag-routes

--- Main/routes/api/tag-routes.js
+++ Main/routes/api/tag-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { rawAttributes } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
@@ -29,12 +30,28 @@ try {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({tag_name: req.body.tag_name});
+    res.status(200).json(tagData => res.json(tagData));
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, { where: {id: req.params.id}});
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
