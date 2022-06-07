8b44a54d4caf23ac3814f64e642a5c8b537fc0b9
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-07 17:25:41 +1000

    one product error fixed

--- Main/routes/api/product-routes.js
+++ Main/routes/api/product-routes.js
@@ -43,11 +43,11 @@ router.get('/', async (req,res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findOne({
+    const productData = await Product.findOne({
       where: {
         id: req.params.id
       },
