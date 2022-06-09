213439e2a96112bf217fce11d8a405539851fd03
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-06-09 17:46:35 +1000

    all work well

--- Main/routes/api/category-routes.js
+++ Main/routes/api/category-routes.js
@@ -51,20 +51,21 @@ router.post('/', async (req, res) => {
 //   }
 // });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id',  (req, res) => {
   // update a category by its `id` value
-  try {
-    const categoryData = await Category.update(req.body, { where: {id: req.params.id}});
-    
-    if (!categoryData[0]) {
-      res.status(404).json({ message: 'No category found with this id'});
-      return;
-    }
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-
+  
+     Category.update(req.body, { where: {id: req.params.id}})
+     .then((categoryData) => {
+      if (!categoryData[0]) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', async (req, res) => {
